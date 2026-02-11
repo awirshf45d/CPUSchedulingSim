c13660da5838b3486ca4c06196595036d7ba3813
pedramfaizy <pedramfaizy96@gmail.com>
2026-02-11 18:09:37 +0330

    Pedram, fiddeling with bpy

--- BlenderCode.py
+++ BlenderCode.py
@@ -1,23 +1,40 @@
 import bpy
-import math
+import math, copy
 
-InputList = [ [1, 6], [80000, 50000], [102500, 20000], [175000, 30000] ] 
+InputList = [ [10000, 60000], [80000, 50000], [102500, 20000], [175000, 30000] ] 
 input_quantum_time: float = 3
 input_cs_time: float = 1
 
 
+
+
+
+original_input_list = copy.deepcopy(InputList)
+scale = 0.0001
+
+for p in original_input_list:
+    p[0] *= scale
+    p[1] *= scale
+
+
+
+
+
+
+
+
+
+
+# (1) Scene preperations like loading the fonts, ...
+
 # ------------------------------------------------------------------
 # IMPORTANT !! : blender has it's own path specifying format as bellow that uses "/" and "//" at the beggining of a relative path.
 # ------------------------------------------------------------------
+# specifying the font path
+font_path = r"//Fonts/JustAnotherHand-Regular.ttf"
 
-
-# Scene preperations like loading the fonts, ...
-def preperations():
-    # specify to your font path
-    font_path = r"//Fonts/JustAnotherHand-Regular.ttf"
-
-    # load the font file
-    font = bpy.data.fonts.load(font_path)
+# load the font file
+font = bpy.data.fonts.load(font_path)
 
 
 
@@ -29,39 +46,54 @@ def preperations():
 
 
 
-pNum = len(InputList)    # the total number of processes we have in our simulation:
-print(pNum)
-# pid = 0
-# y_cordinate = -4
+y_cordinate = -4
 
-# for p in range(pNum):
+for p in original_input_list:    # creating the p_i text objects
 
-#     text_content = f"p{pid}"
+    text_content = f"p{original_input_list.index(p)}"    # numbers the p's based on their index which is the same as pid
 
-#     # Create font curve data
-#     font_curve = bpy.data.curves.new(name=text_content, type='FONT')
-#     font_curve.body = text_content
-#     font_curve.font = font
+    # Create font curve data
+    font_curve = bpy.data.curves.new(name=text_content, type='FONT')
+    font_curve.body = text_content
+    font_curve.font = font
 
-#     # Create object using the curve data
-#     text_obj = bpy.data.objects.new(name=text_content, object_data=font_curve)
+    # Create object using the curve data
+    text_obj = bpy.data.objects.new(name=text_content, object_data=font_curve)
 
-#     # Link object to scene
-#     bpy.context.collection.objects.link(text_obj)
+    # Link object to scene
+    bpy.context.collection.objects.link(text_obj)
 
+    # Set transforms (location, rotation, scale)
+    text_obj.location = (1.4, y_cordinate, 0)    # location (in meters)
+    text_obj.rotation_euler = tuple(map(math.radians, (0, 0, 0)))    # rotation (in degrees)
+    text_obj.scale = (0.5, 0.5, 0.5)    # scale
 
-#     # Set transforms (location, rotation, scale)
-#     text_obj.location = (1.4, y_cordinate, 0)    # location (in meters)
-#     text_obj.rotation_euler = tuple(map(math.radians, (0, 0, 0)))    # rotation (in degrees)
-#     text_obj.scale = (0.5, 0.5, 0.5)    # scale
-
+    # incrementing y
+    y_cordinate += 0.5
 
 
-#     pid += 1
-#     y_cordinate += 0.5
+for p in original_input_list:    # creating the at_i text objects
 
+    text_content = f"{p[0]}"
 
+    # Create font curve data
+    font_curve = bpy.data.curves.new(name=text_content, type='FONT')
+    font_curve.body = text_content
+    font_curve.font = font
 
+    # Create object using the curve data
+    text_obj = bpy.data.objects.new(name=text_content, object_data=font_curve)
+
+    # Link object to scene
+    bpy.context.collection.objects.link(text_obj)
+
+    # Set transforms (location, rotation, scale)
+    text_obj.location = (0, y_cordinate, 0)    # location (in meters)
+    text_obj.rotation_euler = tuple(map(math.radians, (0, 0, 0)))    # rotation (in degrees)
+    text_obj.scale = (0.5, 0.5, 0.5)    # scale
+
+    # incrementing y
+    y_cordinate += 0.5
 
 
 
